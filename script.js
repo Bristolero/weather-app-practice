f55dff331727b886723528094e75b67d607d1441
Bristolero <lennard.rohr@tu-dortmund.de>
2022-01-10 16:24:12 +0100

    final working version

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ async function getWeatherData(str) {
     }
 }
 
+async function getForecastData(str) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${str}&appid=5c3a15a30a363e634cb2a255678865a4`, {mode: 'cors'});
+        if( response.status == 200 ) {
+            const data = await response.json();
+            return data;
+        }
+    } catch (err) {
+        console.error("An error happened");
+    }
+}
+
 const ButtonController = (() => {
 
     const getCityInput = document.getElementById("city-input");
@@ -21,35 +33,51 @@ const ButtonController = (() => {
     getCityButton.addEventListener("click", function () {
         DisplayController.showTodayData();
     })
-    async function handleCitySearch () {
+    getCityInput.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            // Trigger the button element with a click
+            getCityButton.click();
+        }
+    })
+
+    function handleCitySearch () {
         let cityValue = getCityInput.value;
-        console.log(cityValue)
-        let weatherData = await getWeatherData(cityValue);
-        return weatherData;
+        return cityValue;
     }
     return { handleCitySearch, getCityButton };
 })();
 
 const DisplayController = (() => {
 
-    //Container Elements
+    //Container Elements of weather
     const weatherTodayDisplay = document.getElementById("weather-today");
     const weatherOneDay = document.getElementById("1-day");
     const weatherTwoDay = document.getElementById("2-day");
-    const weatherThreeDay = document.getElementById("3-day");
     
 
-    //Content Elements
+    //Content Elements of weather
     const cityNameHeader = document.getElementById("city-name-header");
     const currentTemp = document.getElementById("current-temp");
     const minMaxTemp = document.getElementById("min-max-temp");
     const humidity = document.getElementById("humidity");
     const weather = document.getElementById("weather");
 
+    //Elements of forecast 
+    const tempOneDay = document.getElementById("1-day-temp")
+    const tempTwoDay = document.getElementById("2-day-temp")
+    const sunnyOneDay = document.getElementById("1-day-weather")
+    const sunnyTwoDay = document.getElementById("2-day-weather")
+
+    //Functions of Todays weather
     async function showTodayData() {
-        const weatherData = await ButtonController.handleCitySearch();
-        console.log(weatherData);
+        const searchStr = ButtonController.handleCitySearch();
+
+        weatherData = await getWeatherData(searchStr);
+        forecastData = await getForecastData(searchStr);
+
+
         createTodayElements(weatherData);
+        createForecastElements(forecastData);
     }
 
     function createTodayElements(data) {
@@ -58,9 +86,10 @@ const DisplayController = (() => {
         currentTemp.innerText = LogicController.convertToCelsius(data.main.temp) + "°C";
         minMaxTemp.innerText = LogicController.convertToCelsius(data.main.temp_min) + "°C / " + LogicController.convertToCelsius(data.main.temp_max) + "°C";
         humidity.textContent = "Humidity: " + data.main.humidity + "%" 
-        weather.textContent = data.weather[0].main;
+        weather.textContent = "Weather: " + data.weather[0].main;
 
-        changeWeatherBackground(data.weather[0].main);
+        changeWeatherBackground(data.weather[0].main, weatherTodayDisplay);
+
 
         weatherTodayDisplay.appendChild(cityNameHeader);
         weatherTodayDisplay.appendChild(currentTemp);
@@ -69,24 +98,40 @@ const DisplayController = (() => {
         weatherTodayDisplay.appendChild(weather)
     }
 
-    function changeWeatherBackground(weather) {
-        console.log(weather)
+    function createForecastElements(data) {
+        let hours24Data = data.list[8];
+        let hours48Data = data.list[16];
+
+        tempOneDay.innerText = LogicController.convertToCelsius(hours24Data.main.temp) + "°C";
+        tempTwoDay.innerText = LogicController.convertToCelsius(hours48Data.main.temp) + "°C";
+
+        let hours24Weather = hours24Data.weather[0].main;
+        let hours48Weather = hours48Data.weather[0].main;
+
+        sunnyOneDay.innerText = "Weather: " + hours24Weather
+        sunnyTwoDay.innerText = "Weather: " + hours48Weather
+        changeWeatherBackground(hours24Weather, weatherOneDay);
+        changeWeatherBackground(hours48Weather, weatherTwoDay);
+    }
+
+
+    function changeWeatherBackground(weather, display) {
         if(weather=="Clouds") {
-            weatherTodayDisplay.style.backgroundImage = "url('./img/cloudy.jpeg')";
-            weatherTodayDisplay.style.color = "var(--header-img)";
+            display.style.backgroundImage = "url('./img/cloudy.jpeg')";
+            display.style.color = "var(--header-img)";
         }
         if(weather=="Rain") {
-            weatherTodayDisplay.style.backgroundImage = "url('./img/rainy.jpg')";
-            weatherTodayDisplay.style.color = "white";
+            display.style.backgroundImage = "url('./img/rainy.jpg')";
+            display.style.color = "white";
         }
         if(weather=="Clear") {
-            weatherTodayDisplay.style.backgroundImage = "url('./img/clear.jpeg')";
-            weatherTodayDisplay.style.color = "black";
+            display.style.backgroundImage = "url('./img/clear.jpeg')";
+            display.style.color = "black";
         }
     }
 
 
-    return { showTodayData, createTodayElements }
+    return { showTodayData, createTodayElements, createForecastElements }
 })();
 
 const LogicController = (() => {
@@ -101,7 +146,9 @@ const LogicController = (() => {
 
 async function createDefaultData () {
     const defaultData = await getWeatherData("Dortmund");
+    const defaultForecast = await getForecastData("Dortmund");
     DisplayController.createTodayElements(defaultData);
+    DisplayController.createForecastElements(defaultForecast);
+
 }
-//console.log(defaultData);
 createDefaultData();
